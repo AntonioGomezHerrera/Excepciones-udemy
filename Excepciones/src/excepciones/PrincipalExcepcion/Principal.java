72ddb4334f8c781654002003036b8d28608ef846
Nokster <noksterdj@gmail.com>
2021-01-15 21:43:34 -0600

    Terminando sentencias try-catch-finally

--- Excepciones/src/excepciones/PrincipalExcepcion/Principal.java
+++ Excepciones/src/excepciones/PrincipalExcepcion/Principal.java
@@ -16,9 +16,20 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int primerNumero = 5;
-		int segundoNumero = 0;
+		int segundoNumero = 5;
 		
-		double resultado = primerNumero / segundoNumero;
+		double resultado = 0.0;
+		
+		try {
+			resultado = primerNumero / segundoNumero;
+		} catch (ArithmeticException e) {
+			// TODO: handle exceptio
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+			System.err.println("Error ocasionado por una division entre 0");
+		} finally {
+			System.out.println("Entnado a finally");
+		}
 		
 		System.out.println("El resultado es: " + resultado);
 	}
