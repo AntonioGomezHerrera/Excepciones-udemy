28d5f750e03807379710ac3e157421029a3d081e
Nokster <noksterdj@gmail.com>
2021-01-15 21:18:52 -0600

    Creacion de proyecto

---
+++ Excepciones/src/excepciones/PrincipalExcepcion/Principal.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package excepciones.PrincipalExcepcion;
+
+/**
+ * @author Nokster
+ * Clase que permite mostrar las funcionalidades de las excepciones
+ *
+ */
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int primerNumero = 5;
+		int segundoNumero = 0;
+		
+		double resultado = primerNumero / segundoNumero;
+		
+		System.out.println("El resultado es: " + resultado);
+	}
+
+}
